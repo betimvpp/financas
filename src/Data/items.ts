8ddac049d56970964ae37b87d1a33f543ed801f2
betimvpp <betimvpp@gmail.com>
2022-10-27 15:18:33 -0300

    first commit

---
+++ src/Data/items.ts
@@ -0,0 +1,29 @@
+import { Item } from "../Types/Item"
+
+export const items: Item[] = [
+    {date: new Date(2022, 9, 11),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.50
+    },
+    {date: new Date(2022, 9, 10),
+        category: 'food',
+        title: 'Burger King',
+        value: 28.50
+    },
+    {date: new Date(2022, 9, 9),
+        category: 'rent',
+        title: 'Alguel',
+        value: 1500
+    },
+    {date: new Date(2022, 9, 15),
+        category: 'salary',
+        title: 'Salário',
+        value: 4500
+    },
+    {date: new Date(2022, 10, 15),
+        category: 'salary',
+        title: 'Salário',
+        value: 4500
+    }
+]
